86c922dbfff4337313ae003521f0a9b4fadb425b
Manuela <batrinu.cristian@icloud.com>
2026-06-07 16:33:00 +0300

    fix syntax

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-.import { useState, useRef, useCallback, useEffect } from "react";
+import { useState, useRef, useCallback, useEffect } from "react";
 import { db } from "./firebase";
 import {
   collection, doc, onSnapshot, setDoc, updateDoc, deleteDoc
